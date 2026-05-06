3bdf8f12957a87380385dc096d8f502125a5ef66
LAKSHMINARASIMHAN P S <lakshminarasimhan.ps@algoshack.com>
2026-05-06 16:31:31 +0530

    updated box

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = ({ addToCart, cartCount }) => {
     },
     {
       id: 2,
-      name: "Sample Product myself",
+      name: "Sample Product 2",
       price: "$29.99",
       icon: "📦📚",
       category: "Boxxx",
