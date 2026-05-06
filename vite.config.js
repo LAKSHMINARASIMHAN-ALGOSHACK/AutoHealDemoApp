90df87cc416e6aa257e48d958ed9378e65e4f7a2
LAKSHMINARASIMHAN P S <lakshminarasimhan.ps@algoshack.com>
2026-05-06 15:52:10 +0530

    update deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/AutoHealDemoApp/",
   plugins: [react()],
+  base: 'AutoHealDemoApp', // Replace 'your-repo-name' with your actual GitHub repository name
 })
